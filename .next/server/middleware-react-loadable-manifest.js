cce2962b4b70fea85a5c2844f5e82858a4d400cd
penaputih <ridwannurbakti@gmail.com>
2023-10-17 19:20:04 +0700

    mengganti foto

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST={"_app.jsx -> react-animated-cursor":{id:4077,files:["static/chunks/588.9213b79c01d0ab56.js"]}};
+self.__REACT_LOADABLE_MANIFEST="{\"_app.jsx -> react-animated-cursor\":{\"id\":\"_app.jsx -> react-animated-cursor\",\"files\":[\"static/chunks/node_modules_react-animated-cursor_dist_index_umd_js.js\"]}}"
